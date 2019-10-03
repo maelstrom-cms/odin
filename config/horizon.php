c7d22faede654437d5f00dd893417a098b9e9a41
Owen Melbourne <owen@stormcreative.co.uk>
2019-10-03 11:35:36 +0100

    🏠 House-keeping - reduce job try attempts to 1 as the next schedule will pick it up

--- config/horizon.php
+++ config/horizon.php
@@ -148,7 +148,7 @@ return [
                 'queue' => ['default'],
                 'balance' => 'simple',
                 'processes' => 10,
-                'tries' => 3,
+                'tries' => 1,
             ],
         ],
 
