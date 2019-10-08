40b4320f3b4c397f27bec98794ca8e92d3a3f26e
Owen Melbourne <owen@stormcreative.co.uk>
2019-10-08 09:07:46 +0100

    🐛 Bug - fix unhandled guzzle exceptions

--- app/Checkers/Uptime.php
+++ app/Checkers/Uptime.php
@@ -30,25 +30,31 @@ class Uptime
         $client = new Client();
 
         $response_time = 3001;
+        $keywordFound = false;
 
-        $response = $client->request('GET', $this->website->url, [
-            'on_stats' => function ($stats) use (&$response_time) {
-                $response_time = $stats->getTransferTime();
-            },
-            'verify' => false,
-            'allow_redirects' => true,
-            'headers' => [
-                'User-Agent' => 'Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/; Odin)'
-            ],
-        ]);
+       try {
+           $response = $client->request('GET', $this->website->url, [
+               'on_stats' => function ($stats) use (&$response_time) {
+                   $response_time = $stats->getTransferTime();
+               },
+               'http_errors' => false,
+               'verify' => false,
+               'allow_redirects' => true,
+               'headers' => [
+                   'User-Agent' => 'Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/; Odin)'
+               ],
+           ]);
 
-        $keywordFound = Str::contains($response->getBody(), $this->website->uptime_keyword);
+           $keywordFound = Str::contains($response->getBody(), $this->website->uptime_keyword);
 
-        if (!$keywordFound && $response->getStatusCode() == '200') {
-            $reason = sprintf('Keyword: %s not found (%d)', $this->website->uptime_keyword, 200);
-        } else {
-            $reason = sprintf('%s (%d)', $response->getReasonPhrase(), $response->getStatusCode());
-        }
+           if (!$keywordFound && $response->isSuccessful()) {
+               $reason = sprintf('Keyword: %s not found (%d)', $this->website->uptime_keyword, 200);
+           } else {
+               $reason = sprintf('%s (%d)', $response->getReasonPhrase(), $response->getStatusCode());
+           }
+       } catch (\Exception $exception) {
+           $reason = $exception->getMessage();
+       }
 
         $scan = new UptimeScan([
             'response_status' => $reason,
