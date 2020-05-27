9d96baaa4a2ecff5ac0303e63a6331395d951ded
Zaher Ghaibeh <zaher@zah.me>
2020-05-27 10:50:26 +0300

    adding node_modules directory too

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     './resources/**/*.js',
     './resources/**/*.blade.php',
+    './node_modules/@maelstrom-cms/toolkit/**/*.js',
   ],
   theme: {},
   variants: {},
